41ee4c55cd7e8ea22de1a3ea5c6f8049433801e6
sjorn3 <sean.r.innes@googlemail.com>
2024-05-15 00:35:25 +0100

    create music metadata file

--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,7 @@
 #![allow(clippy::missing_errors_doc)]
 
 use jwalk::WalkDir;
-use music_cache_derive::derive_data_model;
-use rkyv::{Archive, Deserialize, Serialize};
 use std::path::Path;
-
-#[derive_data_model]
-pub struct SongTags {
-    pub title: Option<String>,
-    pub track_number: Option<u16>,
-}
-
-#[derive_data_model]
-pub struct Song {
-    pub tags: SongTags,
-    // converting a path to a utf8 string might not be valid and there's no Archive instance for PathBuf so just store it as bytes.
-    pub relpath: Vec<u8>,
-}
-
-#[derive_data_model]
-pub struct AlbumTags {
-    pub artist: Option<String>,
-    pub title: Option<String>,
-    pub year: Option<u16>,
-}
-
-#[derive_data_model]
-pub struct Album {
-    pub tags: AlbumTags,
-    pub songs: Vec<Song>,
-}
-
-type AudioTag = Box<dyn audiotags::AudioTag + Send + Sync>;
-
-impl AlbumTags {
-    pub fn read(tag: &AudioTag) -> AlbumTags {
-        AlbumTags {
-            artist: tag.album_artist().map(ToString::to_string),
-            title: tag.album_title().map(ToString::to_string),
-            year: tag.year().and_then(|y| y.try_into().ok()),
-        }
-    }
-}
-
-impl SongTags {
-    pub fn read(tag: &AudioTag) -> SongTags {
-        SongTags {
-            title: tag.title().map(ToString::to_string),
-            track_number: tag.track_number(),
-        }
-    }
-}
-
-impl Song {
-    pub fn read(tag: &AudioTag, relpath: &Path) -> Song {
-        Song {
-            tags: SongTags::read(tag),
-            relpath: relpath.to_path_buf().into_os_string().into_encoded_bytes(),
-        }
-    }
-}
 
 pub fn walk(dir: &Path) {
     let mut count = 0;
@@ -95,3 +37,6 @@ pub mod tests {
 
 pub mod db;
 pub use db::*;
+
+pub mod music_metadata;
+pub use music_metadata::*;
---
+++ src/music_metadata.rs
@@ -0,0 +1,58 @@
+use music_cache_derive::derive_data_model;
+use rkyv::{Archive, Deserialize, Serialize};
+use std::path::Path;
+
+#[derive_data_model]
+pub struct Song {
+    pub tags: SongTags,
+    // converting a path to a utf8 string might not be valid and there's no Archive instance for PathBuf so just store it as bytes.
+    pub relpath: Vec<u8>,
+}
+
+#[derive_data_model]
+pub struct SongTags {
+    pub title: Option<String>,
+    pub track_number: Option<u16>,
+}
+
+pub struct Album {
+    pub tags: AlbumTags,
+    pub songs: Vec<Song>,
+}
+
+#[derive_data_model]
+pub struct AlbumTags {
+    pub artist: Option<String>,
+    pub title: Option<String>,
+    pub year: Option<u16>,
+}
+
+type AudioTag = Box<dyn audiotags::AudioTag + Send + Sync>;
+
+impl AlbumTags {
+    pub fn read(tag: &AudioTag) -> AlbumTags {
+        AlbumTags {
+            artist: tag.album_artist().map(ToString::to_string),
+            title: tag.album_title().map(ToString::to_string),
+            year: tag.year().and_then(|y| y.try_into().ok()),
+        }
+    }
+}
+
+impl SongTags {
+    pub fn read(tag: &AudioTag) -> SongTags {
+        SongTags {
+            title: tag.title().map(ToString::to_string),
+            track_number: tag.track_number(),
+        }
+    }
+}
+
+impl Song {
+    pub fn read(tag: &AudioTag, relpath: &Path) -> Song {
+        Song {
+            tags: SongTags::read(tag),
+            relpath: relpath.to_path_buf().into_os_string().into_encoded_bytes(),
+        }
+    }
+}
