b32e1fb4cea74b82ce39bb016ffc048cf7a932d4
sjorn3 <sean.r.innes@googlemail.com>
2024-05-16 23:59:53 +0100

    mark arbitrary a trait

--- src/tests/common.rs
+++ src/tests/common.rs
@@ -7,8 +7,12 @@ use crate::*;
 
 pub type Result = std::result::Result<(), Box<dyn std::error::Error>>;
 
-impl SongTags {
-    pub fn arbitrary() -> Self {
+pub trait Arbitrary {
+    fn arbitrary() -> Self;
+}
+
+impl Arbitrary for SongTags {
+    fn arbitrary() -> Self {
         Self {
             title: Sentence(1..10).fake(),
             track_number: (0..20).fake(),
@@ -16,8 +20,8 @@ impl SongTags {
     }
 }
 
-impl Song {
-    pub fn arbitrary() -> Self {
+impl Arbitrary for Song {
+    fn arbitrary() -> Self {
         Self {
             tags: SongTags::arbitrary(),
             relpath: FilePath().fake::<String>().into_bytes(),
@@ -25,8 +29,8 @@ impl Song {
     }
 }
 
-impl AlbumTags {
-    pub fn arbitrary() -> Self {
+impl Arbitrary for AlbumTags {
+    fn arbitrary() -> Self {
         Self {
             artist: Name().fake(),
             title: Sentence(1..10).fake(),
@@ -35,8 +39,8 @@ impl AlbumTags {
     }
 }
 
-impl Album {
-    pub fn arbitrary() -> Self {
+impl Arbitrary for Album {
+    fn arbitrary() -> Self {
         Self {
             tags: AlbumTags::arbitrary(),
             songs: (0..(2..20).fake()).map(|_| Song::arbitrary()).collect(),
